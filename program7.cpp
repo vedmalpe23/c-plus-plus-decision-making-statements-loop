a0faa2744a6b423f14458269959396ede0d5d5b0
ved malpe <vedmalpe21@gmail.com>
2025-10-07 20:16:53 +0530

    Create program7.cpp

---
+++ program7.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int i,j,k;
+    int n=5;
+    for(i=1;i<=n;i++)
+    {
+          for(k=1;k<=n-i;k++)
+          {
+              cout<<" ";
+          }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+}
+/*
+output:
+    *
+   **
+  ***
+ ****
+*****
+  */
