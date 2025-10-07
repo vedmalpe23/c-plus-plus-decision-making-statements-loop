bf9924312ab602fd0d46a5ac85b89202af278e0e
ved malpe <vedmalpe21@gmail.com>
2025-10-07 20:12:09 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,18 @@
+//Printing same word five times
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cout<<"Symbiosis Institute of Technology("<<i+1<<")"<<endl;
+    }
+}
+/*output:
+Symbiosis Institute of Technology(1)
+Symbiosis Institute of Technology(2)
+Symbiosis Institute of Technology(3)
+Symbiosis Institute of Technology(4)
+Symbiosis Institute of Technology(5)
+*/
